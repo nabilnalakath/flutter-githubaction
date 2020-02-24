ea2f8ec66150bcd4064ea9c9bea4595583132965
nabil.nalakath <nabilnalakath.git@gmail.com>
2020-02-24 23:00:45 +0530

    Changes: Github CLI test

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Git Hub Actions Sample App'),
+      home: MyHomePage(title: 'Git Hub Actions Sample App with Github Cli'),
     );
   }
 }
