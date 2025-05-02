94c49d3335967c08b425b20143e92653ea4ce497
Nabil Mohammed Nalakath <32065631+nabilnalakath@users.noreply.github.com>
2025-05-02 20:12:15 +0000

    regenerate Android & iOS folders with V2 embedding

--- android/app/src/main/kotlin/com/example/flutter_githubaction/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_githubaction/MainActivity.kt
@@ -2,6 +2,4 @@ package com.example.flutter_githubaction
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-    // No onCreate or GeneratedPluginRegistrant calls needed
-}
+class MainActivity : FlutterActivity()
